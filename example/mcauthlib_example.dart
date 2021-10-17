4768f2a259040c107d2cf56218c7549b4355de51
GiftShower <hj.samuel0516@gmail.com>
2021-10-17 21:15:15 +0900

    Updated example.

--- example/mcauthlib_example.dart
+++ example/mcauthlib_example.dart
@@ -1,6 +1,24 @@
 import 'package:mcauthlib/mcauthlib.dart';
 
-void main() {
-  //TODO
+void main() async {
+  //This example is how to authenticate, refresh, validate and sign out.
 
+  //First create MojangAuth instance.
+  final auth = MojangAuth();
+  //Then, create User with authtype keyword.
+  //The first parameter is email and the second one is password.
+  var user = User.authType("lorem@ipsum.com", "dolorsitamet");
+
+  //await until authentication return map with
+  //Response type, clientToken, accessToken, player uuid.
+  var result = await auth.authenticate(user);
+
+  //If a request was successful the response type should be
+  //ResponseType.success
+  //if not, it will return ResponseType.failure with error Type.
+  if(result['responseType'] == ResponseType.success){
+    user.clientToken = result['clientToken'];
+    user.accessToken = result['accessToken'];
+  }
+  //TODO: More.
 }
